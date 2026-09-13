5afe0b8528acb6cc6eeac896a3f59f20bfd8961b
shivansh3960 <opshivansh2008@gmail,com>
2026-09-13 13:14:10 +0000

    Day4

---
+++ Day4/q7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+
+int main() {
+    int a, b;
+
+    printf("Enter no : a\n ");
+    scanf ("%d", &a);
+    printf ("enter no : b\n");
+        scanf("%d", &b);
+        a= a+b;
+        b= a-b;
+        a= a-b;
+        printf ("a is %d\n", a);
+printf ("b is %d\n", b);
+return 0;
+}
