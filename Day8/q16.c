4a8a5df51fd4eb9278026aa912a89b6c43ac12a1
shivansh3960 <opshivansh2008@gmail,com>
2026-09-14 05:13:44 +0000

    Day8

---
+++ Day8/q16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main (){
+int  a, b,c ;
+printf ("enter 3 no :");
+scanf ("%d %d %d",&a,&b,&c);
+if (a>b && a>c){
+    printf ("%d is   the largest",a);
+
+}
+if (b>c && b>a)
+{
+    printf ("%d is the largest",b);
+
+}
+else 
+{
+    printf ("%d is the largest",c);
+    return 0;
+}
+
+
+
+
+}
